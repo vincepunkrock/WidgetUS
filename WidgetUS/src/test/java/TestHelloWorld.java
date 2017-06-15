38bf0a0c8246f4e3d72145f6090db370b05b341a
Amrita Malan <amrita.malan@gmail.com>
2017-06-15 11:20:44 -0400

    JUnit

--- WidgetUS/src/test/java/TestHelloWorld.java
+++ WidgetUS/src/test/java/TestHelloWorld.java
@@ -1,7 +1,14 @@
-package PACKAGE_NAME;
+import org.junit.Assert;
+import org.junit.Test;
 
 /**
  * Created by EnZo on 2017-06-15.
  */
 public class TestHelloWorld {
+
+    @Test
+    public void firstTest(){
+
+        Assert.assertTrue(true);
+    }
 }
