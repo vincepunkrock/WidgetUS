0187d245fdc90fa2aec7c872dd7eafffca4b4428
Vincent Gagnon <vincent.gagnon93@gmail.com>
2017-06-28 14:09:54 -0400

    premier draft de meteo fonctionnelle

--- widgetus-angular/src/app/meteo.service.ts
+++ widgetus-angular/src/app/meteo.service.ts
@@ -1,8 +1,16 @@
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs/Observable';
+import { Http } from '@angular/http';
 
 @Injectable()
 export class MeteoService {
 
-  constructor() { }
+  constructor(private http: Http) { }
+
+  getWeather(cityQuery: string, units: string): Observable<any> {
+    return this.http.get("http://api.openweathermap.org/data/2.5/weather?q="+ cityQuery +"&appid=f2d3f236b9b8518f948a3fffbe2beb2c&units="+units+"&lang=fr")
+    .map((res) => res.json())
+    .catch((err)=> Observable.throw(err));
+  }
 
 }
--- widgetus-angular/src/app/widget-meteo/widget-meteo.component.ts
+++ widgetus-angular/src/app/widget-meteo/widget-meteo.component.ts
@@ -1,15 +1,53 @@
 import { Component, OnInit } from '@angular/core';
+import { MeteoService } from '../meteo.service';
+import { WeatherIcons } from './WeatherIcons';
 
 @Component({
   selector: 'gridster-widget-meteo',
   templateUrl: './widget-meteo.component.html',
-  styleUrls: ['./widget-meteo.component.css']
+  styleUrls: ['./widget-meteo.component.css'],
+  providers: [ MeteoService ]
 })
 export class WidgetMeteoComponent implements OnInit {
 
-  constructor() { }
+  iconName: string = "";
+  temp: number;
+  cityQuery: string = "Sherbrooke, qc";
+  cityResult: string = "";
+  desc: string;
+
+  constructor(private meteoService: MeteoService) { }
 
   ngOnInit() {
+    this.loadWeather();
+  }
+
+  loadWeather() {
+     this.meteoService.getWeather(this.cityQuery, "metric").subscribe(res => {
+      if(res){
+          this.iconName = this.buildIconName(res.weather[0].id);
+          this.temp = res.main.temp;
+          this.cityResult = res.name;
+          this.desc = res.weather.description;
+      }
+    }, err => {
+
+    });
+  }
+
+  buildIconName(code): string {
+    let prefix = 'wi wi-';
+    let icon = WeatherIcons.WEATHER_ICONS[code].icon;
+
+    // If we are not in the ranges mentioned above, add a day/night prefix.
+    if (!(code > 699 && code < 800) && !(code > 899 && code < 1000)) {
+      icon = 'day-' + icon;
+    }
+
+    // Finally tack on the prefix.
+    icon = prefix + icon;
+
+    return icon;
   }
 
 }
