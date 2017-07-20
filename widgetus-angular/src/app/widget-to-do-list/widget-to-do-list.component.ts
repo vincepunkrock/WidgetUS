6ce515ac1a709997cbd39cce475d03c66ddd5f7a
Adriatik Sermaxhaj <adri.sermax@gmail.com>
2017-07-20 02:13:54 -0400

    no message

--- widgetus-angular/src/app/widget-to-do-list/widget-to-do-list.component.ts
+++ widgetus-angular/src/app/widget-to-do-list/widget-to-do-list.component.ts
@@ -1,10 +1,13 @@
 import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
+import {HttpService} from '../http.service';
+import * as _ from 'underscore';
 
 
 @Component({
   selector: 'gridster-widget-to-do-list',
   templateUrl: './widget-to-do-list.component.html',
-  styleUrls: ['./widget-to-do-list.component.css']
+  styleUrls: ['./widget-to-do-list.component.css'],
+  providers: [HttpService]
 })
 export class WidgetToDoListComponent implements OnInit {
   @Output() removed = new EventEmitter();
@@ -15,7 +18,7 @@ export class WidgetToDoListComponent implements OnInit {
   ToDoList: Array<String>;
 
 
-  constructor() { }
+  constructor(private _httpService: HttpService) { }
 
   ngOnInit() {
     this.ToDoNote = '';
@@ -35,7 +38,13 @@ export class WidgetToDoListComponent implements OnInit {
   onChangeItemNote(e,item){
     if(e === 'supprimer')
     {
-      this.ToDoList.splice(this.ToDoList.indexOf(item), 1);
+      this._httpService.deleteToDoTask(638)
+      .subscribe(
+        data => this.ToDoList.splice(this.ToDoList.indexOf(item), 1),
+        // error => alert(error),
+        error => this.ToDoList.splice(this.ToDoList.indexOf(item), 1),
+        () => console.log('Finished')
+      );
       // update BD
     }
     else if(e === 'complété'){
@@ -47,7 +56,14 @@ export class WidgetToDoListComponent implements OnInit {
   }
 
   onAddNote(){
-    this.ToDoList.push(this.ToDoNote);
     // update BD
+    this._httpService.postToDoTask(638, this.ToDoNote, 0)
+      .subscribe(
+        data => this.ToDoList.push(this.ToDoNote),
+        // error => alert(error),
+        error => this.ToDoList.push(this.ToDoNote),
+        () => console.log('Finished')
+      );
   }
+
 }
