e1ad48f6b3585508ac4b18f7a6e1cc848bb2f977
pBalthazar <balthy55@gmail.com>
2017-07-21 09:21:54 -0400

    Input text if url is not defined

--- widgetus-angular/src/app/widget-custom/widget-custom.component.ts
+++ widgetus-angular/src/app/widget-custom/widget-custom.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit, Output, Input, EventEmitter } from '@angular/core';
 import { DomSanitizer, SafeResourceUrl} from '@angular/platform-browser';
+import * as _ from 'underscore';
 
 @Component({
   selector: 'gridster-widget-custom',
@@ -12,17 +13,27 @@ export class WidgetCustomComponent implements OnInit {
   @Output() name: string;
   @Input() widget;
 
+  tmp_url;
   url: SafeResourceUrl;
+  urlOk;
 
   constructor(private sanitizer: DomSanitizer) { }
 
   ngOnInit() {
     this.name = 'Custom';
-    // this.url = "https://www.gel.usherbrooke.ca/horarius/";
-    this.url = this.sanitizer.bypassSecurityTrustResourceUrl('https://www.gel.usherbrooke.ca/horarius/');
+    // this.tmp_url = 'https://drive.google.com/embeddedfolderview?id=0B7R2gk2Et7gCdks3R1g2Q1ZDWjg#list';
+    // this.tmp_url = 'https://www.gel.usherbrooke.ca/s6info/e17/doc/app5/';
+    // this.tmp_url = '';
+    this.urlOk = !_.isEmpty(this.tmp_url);
+    this.url = this.sanitizer.bypassSecurityTrustResourceUrl(this.tmp_url);
   }
 
   removeItem(e) {
     this.removed.emit(e);
+  }
+
+  updateUrl() {
+    this.url = this.sanitizer.bypassSecurityTrustResourceUrl(this.tmp_url);
+    this.urlOk = !_.isEmpty(this.tmp_url);
   }
 }
